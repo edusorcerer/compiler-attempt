82264237b99a1c502664cddbf1c5563b38d012eb
edusorcerer <eduardofidera@gmail.com>
2020-04-01 19:00:33 -0300

    Add code generator

--- src/compiler.js
+++ src/compiler.js
@@ -324,9 +324,50 @@ function getTransformedAbstractSyntaxTree(ast) {
   return newAst
 }
 
+/**
+ * Calls itself recursively to print each node in the tree into a single string
+ *
+ * @param {Object} node
+ */
+function getGeneratedCode(node) {
+  switch (node.type) {
+    /** map through each node in the body of the program, calling the getGeneratedCode and then join them */
+    case "Program":
+      return node.body.map(getGeneratedCode).join("\n")
+
+    /** call the getGeneratedCode on the nested expression and add a semicolon */
+    case "ExpressionStatement":
+      return getGeneratedCode(node.expression) + ";"
+
+    /** print the callee, add the parenthesis and generate code for each node in the arguments, while joining them with a comma */
+    case "CallExpression":
+      return (
+        getGeneratedCode(node.callee) +
+        "(" +
+        node.arguments.map(getGeneratedCode).join(", ") +
+        ")"
+      )
+
+    case "Identifier":
+      return node.name
+
+    case "NumberLiteral":
+      return node.value
+
+    /** add quotes for strings */
+    case "StringLiteral":
+      return '"' + node.value + '"'
+
+    default:
+      throw new TypeError(node.type)
+  }
+}
+
 const tokens = getTokens("(add 2 (subtract 2 4))")
 
 const ast = getAbstractSyntexTree(tokens)
 
 const newAst = getTransformedAbstractSyntaxTree(ast)
-console.log(JSON.stringify(newAst))
+
+const code = getGeneratedCode(newAst)
+console.log(code)
